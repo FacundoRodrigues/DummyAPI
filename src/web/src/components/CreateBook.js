7c228415ef5b66b60112c0afe570375fc3dfba23
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-10-02 17:36:16 -0300

    2-10-23: configura eslint y corrige errores

--- src/web/src/components/CreateBook.js
+++ src/web/src/components/CreateBook.js
@@ -1,75 +1,74 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 export const CreateBook = () => {
-    const INITIAL_VALUES = {
-        title: '',
-        author: ''
-    }
+	const INITIAL_VALUES = {
+		title: '',
+		author: '',
+	}
 
-    const [formState, setFormState] = useState(INITIAL_VALUES)
+	const [formState, setFormState] = useState(INITIAL_VALUES)
 
-    const { title, author } = formState
+	const { title, author } = formState
 
-    const handleSubmit = ( e ) => {
-        e.preventDefault()
+	const handleSubmit = (e) => {
+		e.preventDefault()
 
-        try 
-        {
-            console.log(title)
-            console.log(author)
-            fetch("http://localhost:5199/Bookitems", {
-                method: "POST",
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    title: title,
-                    author: author
-                })
-            })
-            .then(response => response.json())
-            .then(data => {
-                if(data.status !== 200) console.log("Some error occured");
-                console.log('OK')
-                setFormState(INITIAL_VALUES)
-            })
-        } catch (err) {
-            console.log(err);
-        }
-    }
+		try {
+			console.log(title)
+			console.log(author)
+			fetch('http://localhost:5199/Bookitems', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					title: title,
+					author: author,
+				}),
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					if (data.status !== 200) console.log('Some error occured')
+					console.log('OK')
+					setFormState(INITIAL_VALUES)
+				})
+		} catch (err) {
+			console.log(err)
+		}
+	}
 
-    const handleInputChange = ({target}) => {
-        setFormState({
-            ...formState,
-            [target.name] : target.value
-        })
-    }
+	const handleInputChange = ({ target }) => {
+		setFormState({
+			...formState,
+			[target.name]: target.value,
+		})
+	}
 
-    return (
-        <>
-            <h1>Create Book</h1>
-            <hr />
+	return (
+		<>
+			<h1>Create Book</h1>
+			<hr />
 
-            <form onSubmit={ handleSubmit }>
-                <input
-                    type='text'
-                    name='title'
-                    value={title}
-                    placeholder='Title'
-                    onChange={ handleInputChange }
-                />
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					name="title"
+					value={title}
+					placeholder="Title"
+					onChange={handleInputChange}
+				/>
 
-                <input
-                    type='text'
-                    name='author'
-                    value={author}
-                    placeholder='Author'
-                    onChange={ handleInputChange }
-                />
+				<input
+					type="text"
+					name="author"
+					value={author}
+					placeholder="Author"
+					onChange={handleInputChange}
+				/>
 
-                <button>Save</button>
-            </form>
-        </>
-    )
+				<button>Save</button>
+			</form>
+		</>
+	)
 }
--- src/web/src/index.js
+++ src/web/src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BookList } from './components/BookList';
-import { CreateBook } from './components/CreateBook';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BookList } from './components/BookList'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <BookList  />
-);
+	<BookList  />
+)
