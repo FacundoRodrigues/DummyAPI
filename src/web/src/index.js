c1c02337b0d9cbfbcbcf70acdfc58b184c249bf2
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-10-02 15:50:58 -0300

    2-10-23: installa eslint y standar

--- src/web/src/index.js
+++ src/web/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import { BookList } from './components/BookList';
+import { BookList } from './components/BookList';
 import { CreateBook } from './components/CreateBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <CreateBook />
+    <BookList  />
 );
