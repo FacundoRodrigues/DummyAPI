4947b06b99061b9b35c66d100be66cf5916cf029
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-10-02 17:41:25 -0300

    2-10-23: termina fixear identacion

--- src/web/src/components/BookList.js
+++ src/web/src/components/BookList.js
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react'
 
 export const BookList = () => {
-    const [book, setBook] = useState([])
+	const [book, setBook] = useState([])
 
-    useEffect( () => {
-        fetch('http://localhost:5199/Bookitems')
-            .then(res => res.json())
-            .then(data => setBook(data))
-    },[])
+	useEffect( () => {
+		fetch('http://localhost:5199/Bookitems')
+			.then(res => res.json())
+			.then(data => setBook(data))
+	},[])
 
-    return (
-        <>
-            <h1>Book List</h1>
-            <hr />
+	return (
+		<>
+			<h1>Book List</h1>
+			<hr />
 
-            { 
-                book.map( item => 
-                    <div key={item.id}>
-                        <p>
-                            <strong>{ item.title }</strong> - <cite>{ item.author }</cite>
-                        </p>
-                        <cite></cite>
-                    </div>
-                ) 
-            }
-        </>
-    )
+			{ 
+				book.map( item => 
+					<div key={item.id}>
+						<p>
+							<strong>{ item.title }</strong> - <cite>{ item.author }</cite>
+						</p>
+						<cite></cite>
+					</div>
+				) 
+			}
+		</>
+	)
 }
