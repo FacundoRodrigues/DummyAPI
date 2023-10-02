08bc04de2d49125673904142bc95abad4da9afe0
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-10-02 11:05:36 -0300

    2-10-23: Minimal API Books

---
+++ DummyAPI/DummyAPI/Context/BookDb.cs
@@ -0,0 +1,13 @@
+﻿using DummyAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyAPI.Context
+{
+    public class BookDb : DbContext
+    {
+        public BookDb(DbContextOptions<BookDb> options)
+            : base(options) { }
+
+        public DbSet<Book> Books => Set<Book>();
+    }
+}
---
+++ DummyAPI/DummyAPI/Program.cs
@@ -0,0 +1,54 @@
+using DummyAPI.Context;
+using DummyAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<BookDb>(opt => opt.UseInMemoryDatabase("BookList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+app.MapGet("/Bookitems", async (BookDb db) =>
+    await db.Books.ToListAsync());
+
+app.MapGet("/Bookitems/{id}", async (int id, BookDb db) =>
+    await db.Books.FindAsync(id)
+        is Book Book
+        ? Results.Ok(Book)
+        : Results.NotFound());
+
+app.MapPost("/Bookitems", async (Book Book, BookDb db) =>
+{
+    db.Books.Add(Book);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/Bookitems/{Book.Id}", Book);
+});
+
+app.MapPut("/Bookitems/{id}", async (int id, Book inputBook, BookDb db) =>
+{
+    var Book = await db.Books.FindAsync(id);
+
+    if (Book is null) return Results.NotFound();
+
+    Book.Title = inputBook.Title;
+    Book.Author = inputBook.Author;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/Bookitems/{id}", async (int id, BookDb db) =>
+{
+    if (await db.Books.FindAsync(id) is Book Book)
+    {
+        db.Books.Remove(Book);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
+app.Run();
